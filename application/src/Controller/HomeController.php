174a53c59c49cf3611a269d05cbe8a576fa2e665
ArtZiom <artziom@gmail.com>
2022-12-01 22:51:33 +0100

    Create user budgets list

--- application/src/Controller/HomeController.php
+++ application/src/Controller/HomeController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\BudgetRepository;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(BudgetRepository $budgetRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $user = $this->getUser();
+        $budgets = $budgetRepository->findBy(['owner' => $user]);
+        return $this->render('home/index.html.twig', [
+            'budgets' => $budgets
+        ]);
     }
 
     #[Route('/logout', name: 'app_logout')]
